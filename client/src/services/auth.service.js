27a5b91c1d9874a829a23f9c1287044daf22cf49
tinoMukaro <vamukaro3@gmail.com>
2026-03-01 11:57:44 +0200

    added docker files

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:5000",
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,10 @@ import app from './app.js';
 import 'dotenv/config.js';
 import { startDealCron } from './config/deal.cron.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 startDealCron();
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
